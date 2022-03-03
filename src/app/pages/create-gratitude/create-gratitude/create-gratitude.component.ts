2a92789d301cad54aca844e617a611c35783a9fc
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2022-03-03 21:11:40 +0200

    added fields state

--- src/app/pages/create-gratitude/create-gratitude/create-gratitude.component.ts
+++ src/app/pages/create-gratitude/create-gratitude/create-gratitude.component.ts
@@ -7,11 +7,22 @@ import { DappInjectorService } from 'angular-web3';
   styleUrls: ['./create-gratitude.component.scss']
 })
 export class CreateGratitudeComponent implements OnInit {
-
+  details = {
+    name:'',
+    gratitude:'',
+  }
   constructor(private dappInjectorService:DappInjectorService) { }
 
   ngOnInit(): void {
   // const myContract = this.dappInjectorService.config.contracts['myContract'].runFunction('mint',[])
   }
-
+  submitNft(): void {
+    //Upload Files to IPFS & Mint NFT
+    const {name,gratitude} = this.details;
+    if (name&&gratitude)
+    alert(`
+    Name: ${this.details.name}
+    Gratitude: ${this.details.gratitude}
+    `);
+  }
 }
