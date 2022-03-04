58aa8f3a4fd226d0918782d7f2be4c4cf8d9755e
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-03-03 20:54:59 -0700

    image filter toggle

--- src/app/pages/create-gratitude/create-gratitude/create-gratitude.component.ts
+++ src/app/pages/create-gratitude/create-gratitude/create-gratitude.component.ts
@@ -25,4 +25,24 @@ export class CreateGratitudeComponent implements OnInit {
     Gratitude: ${this.details.gratitude}
     `);
   }
+  isFilter: boolean = false;
+
+  clickEvent(){
+    this.isFilter = !this.isFilter;       
 }
+}
+
+// image filter
+
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+// const image = document.getElementById('source');
+// 
+// image.addEventListener('load', e => {
+//   // Draw unfiltered image
+//   ctx.drawImage(image, 0, 0, image.width * .6, image.height * .6);
+// 
+//   // Draw image with filter
+//   ctx.filter = 'contrast(1.4) sepia(1) drop-shadow(-9px 9px 3px #e81)';
+//   ctx.drawImage(image, 400, 0, -image.width * .6, image.height * .6);
+// });
