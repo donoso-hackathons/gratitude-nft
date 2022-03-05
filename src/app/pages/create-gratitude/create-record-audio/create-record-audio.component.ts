f1b17154c05bb4d7521a0b0baea5e3d28f0c947b
jdonosocoding <javier.donoso@nuilea.com>
2022-03-05 13:50:56 +0100

    small fix

--- src/app/pages/create-gratitude/create-record-audio/create-record-audio.component.ts
+++ src/app/pages/create-gratitude/create-record-audio/create-record-audio.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { DappInjectorService, NotifierService } from 'angular-web3';
+import { Contract } from 'ethers';
 import { IpfsService } from '../../ipfs/ipfs-service';
 
 @Component({
@@ -9,8 +10,8 @@ import { IpfsService } from '../../ipfs/ipfs-service';
   templateUrl: './create-record-audio.component.html',
   styleUrls: ['./create-record-audio.component.scss']
 })
-export class CreateRecordAudioComponent implements OnInit {
-
+export class CreateRecordAudioComponent implements AfterViewInit {
+  gratitudeContract: Contract;
   constructor(
     private store:Store,
     private cd:ChangeDetectorRef,
@@ -20,7 +21,9 @@ export class CreateRecordAudioComponent implements OnInit {
     private router: Router) {
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.gratitudeContract =  this.dappInjectorService.config.contracts['myContract'].contract
+
   }
 
 }
--- src/app/pages/create-gratitude/create-record-video/create-record-video.component.ts
+++ src/app/pages/create-gratitude/create-record-video/create-record-video.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { DappInjectorService, NotifierService } from 'angular-web3';
+import { Contract } from 'ethers';
+import { IpfsService } from '../../ipfs/ipfs-service';
 
 @Component({
   selector: 'create-record-video',
   templateUrl: './create-record-video.component.html',
   styleUrls: ['./create-record-video.component.scss']
 })
-export class CreateRecordVideoComponent implements OnInit {
+export class CreateRecordVideoComponent  implements AfterViewInit {
+  gratitudeContract: Contract;
+  constructor(
+    private store:Store,
+    private cd:ChangeDetectorRef,
+    public ipfsService: IpfsService, 
+    private dappInjectorService:DappInjectorService,
+    private notifierService: NotifierService,
+    private router: Router) {
+  }
 
-  constructor() { }
+  ngAfterViewInit(): void {
+    this.gratitudeContract =  this.dappInjectorService.config.contracts['myContract'].contract
 
-  ngOnInit(): void {
   }
 
 }
