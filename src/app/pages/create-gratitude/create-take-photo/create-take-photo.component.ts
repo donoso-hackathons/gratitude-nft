630cab0c9ef2490b376b3290ae7dab1c902beeca
jdonosocoding <javier.donoso@nuilea.com>
2022-03-05 07:22:16 +0100

    fix:serror

--- src/app/pages/create-gratitude/create-take-photo/create-take-photo.component.ts
+++ src/app/pages/create-gratitude/create-take-photo/create-take-photo.component.ts
@@ -1,15 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import { WebcamInitError, WebcamImage, WebcamUtil } from 'ngx-webcam';
+import { Subject, Observable } from 'rxjs';
+import { IpfsService } from '../../ipfs/ipfs-service';
 
 @Component({
   selector: 'photo',
   templateUrl: './create-take-photo.component.html',
-  styleUrls: ['./create-take-photo.component.scss']
+  styleUrls: ['./create-take-photo.component.scss'],
 })
 export class CreateTakePhotoComponent implements OnInit {
+  // toggle webcam on/off
+  public showWebcam = false;
+  public allowCameraSwitch = true;
+  public multipleWebcamsAvailable = false;
+  public deviceId: string;
+  public videoOptions: MediaTrackConstraints = {
+    // width: {ideal: 1024},
+    // height: {ideal: 576}
+  };
+  public errors: WebcamInitError[] = [];
 
-  constructor() { }
+  // latest snapshot
+  public webcamImage: WebcamImage = null;
 
-  ngOnInit(): void {
+  // webcam snapshot trigger
+  private trigger: Subject<void> = new Subject<void>();
+
+
+constructor(public ipfsService: IpfsService) {
+
+}
+
+  public ngOnInit(): void {
+    WebcamUtil.getAvailableVideoInputs().then(
+      (mediaDevices: MediaDeviceInfo[]) => {
+        this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length > 1;
+      }
+    );
   }
 
+ async mintNft() {
+   console.log(this.webcamImage)
+    const result = await this.ipfsService.add(this.webcamImage.imageAsDataUrl);
+    console.log(result)
+    console.log(`https://ipfs.io/ipfs/${result[0].hash}`)
+  }
+
+  public triggerSnapshot(): void {
+    this.trigger.next();
+  }
+
+  public toggleWebcam(): void {
+    this.showWebcam = !this.showWebcam;
+  }
+
+  public handleInitError(error: WebcamInitError): void {
+    this.errors.push(error);
+  }
+
+  public handleImage(webcamImage: WebcamImage): void {
+    console.info('received webcam image', webcamImage);
+    this.webcamImage = webcamImage;
+    this.showWebcam = false;
+  }
+
+  public cameraWasSwitched(deviceId: string): void {
+    console.log('active device: ' + deviceId);
+    this.deviceId = deviceId;
+  }
+
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
 }
