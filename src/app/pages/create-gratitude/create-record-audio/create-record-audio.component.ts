dfe2e2c2e54f6c2ed690194f1d6841ee6c450fc1
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-03-05 07:32:50 -0700

    audio record interface make sure to install new dependencies

--- src/app/pages/create-gratitude/create-record-audio/create-record-audio.component.ts
+++ src/app/pages/create-gratitude/create-record-audio/create-record-audio.component.ts
@@ -5,6 +5,10 @@ import { DappInjectorService, NotifierService } from 'angular-web3';
 import { Contract } from 'ethers';
 import { IpfsService } from '../../ipfs/ipfs-service';
 
+declare var $: any;
+import * as RecordRTC from 'recordrtc';
+import { DomSanitizer } from '@angular/platform-browser';
+
 @Component({
   selector: 'create-record-audio',
   templateUrl: './create-record-audio.component.html',
@@ -12,18 +16,22 @@ import { IpfsService } from '../../ipfs/ipfs-service';
 })
 export class CreateRecordAudioComponent implements AfterViewInit {
   gratitudeContract: Contract;
+  title = 'micRecorder';
   constructor(
     private store:Store,
     private cd:ChangeDetectorRef,
     public ipfsService: IpfsService, 
     private dappInjectorService:DappInjectorService,
     private notifierService: NotifierService,
+    private domSanitizer: DomSanitizer,
     private router: Router) {
   }
 
   ngAfterViewInit(): void {
     this.gratitudeContract =  this.dappInjectorService.config.contracts['myContract'].contract
 
+
+    
   //   navigator.mediaDevices.getUserMedia({ audio: true })
   // .then(stream => {
   //   const mediaRecorder = new MediaRecorder(stream);
@@ -31,5 +39,67 @@ export class CreateRecordAudioComponent implements AfterViewInit {
   // });
  
   }
+
+//Lets declare Record OBJ
+record;
+//Will use this flag for toggeling recording
+recording = false;
+//URL of Blob
+url;
+error;
+constr
+  
+sanitize(url: string) {
+return this.domSanitizer.bypassSecurityTrustUrl(url);
+  }
+  
+  /**
+* Start recording.
+*/
+initiateRecording() {
+this.recording = true;
+let mediaConstraints = {
+video: false,
+audio: true
+};
+navigator.mediaDevices.getUserMedia(mediaConstraints).then(this.successCallback.bind(this), this.errorCallback.bind(this));
+}
+/**
+* Will be called automatically.
+*/
+successCallback(stream) {
+var options = {
+mimeType: "audio/wav",
+numberOfAudioChannels: 1,
+sampleRate: 16000,
+};
+//Start Actuall Recording
+var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
+this.record = new StereoAudioRecorder(stream, options);
+this.record.record();
+}
+/**
+* Stop recording.
+*/
+stopRecording() {
+this.recording = false;
+this.record.stop(this.processRecording.bind(this));
+}
+/**
+* processRecording Do what ever you want with blob
+* @param  {any} blob Blog
+*/
+processRecording(blob) {
+this.url = URL.createObjectURL(blob);
+console.log("blob", blob);
+console.log("url", this.url);
+}
+/**
+* Process Error.
+*/
+errorCallback(error) {
+this.error = 'Can not play audio in your browser';
+}
+ngOnInit() {}
 
 }
